dee7d8043881e5d16873078eea2a660303ae8571
Andrei Degtiarev <adegtiarev@gmail.com>
2023-12-01 16:30:19 -0300

    update

--- karate-core/src/main/java/com/intuit/karate/core/Embed.java
+++ karate-core/src/main/java/com/intuit/karate/core/Embed.java
@@ -1,4 +1,24 @@
 /*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Project: https://karatelabs.github.io/karate
+*    Release: https://github.com/karatelabs/karate/releases/tag/v1.2.1.RC1
+*    Source File: Embed.java
+*    
+*    Copyrights:
+*      copyright 2018 intuit inc
+*    
+*    Licenses:
+*      MIT License
+*      SPDXId: MIT
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
+/*
  * The MIT License
  *
  * Copyright 2018 Intuit Inc.
