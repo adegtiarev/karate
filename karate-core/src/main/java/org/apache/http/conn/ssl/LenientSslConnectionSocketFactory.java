8b9b808c59767e57288613c7bf5cddbe370dc6b3
Andrei Degtiarev <adegtiarev@gmail.com>
2023-12-01 16:29:27 -0300

    update

--- karate-core/src/main/java/org/apache/http/conn/ssl/LenientSslConnectionSocketFactory.java
+++ karate-core/src/main/java/org/apache/http/conn/ssl/LenientSslConnectionSocketFactory.java
@@ -1,4 +1,24 @@
 /*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Project: https://karatelabs.github.io/karate
+*    Release: https://github.com/karatelabs/karate/releases/tag/v0.9.6
+*    Source File: LenientSslConnectionSocketFactory.java
+*    
+*    Copyrights:
+*      copyright 2017 intuit inc
+*    
+*    Licenses:
+*      MIT License
+*      SPDXId: MIT
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
+/*
  * The MIT License
  *
  * Copyright 2017 Intuit Inc.
