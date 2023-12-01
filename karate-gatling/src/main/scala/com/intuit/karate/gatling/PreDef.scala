4502abd9b22d3e506fc7bb95876e69d2d5081994
Andrei Degtiarev <adegtiarev@gmail.com>
2023-12-01 16:31:25 -0300

    update

--- karate-gatling/src/main/scala/com/intuit/karate/gatling/PreDef.scala
+++ karate-gatling/src/main/scala/com/intuit/karate/gatling/PreDef.scala
@@ -1,3 +1,25 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Project: https://karatelabs.github.io/karate
+*    Release: https://github.com/karatelabs/karate/releases/tag/v1.3.1
+*    Source File: PreDef.scala
+*    
+*    Copyrights:
+*      copyright 2017 intuit inc
+*      copyright 2020 pthomas3
+*      copyright 2011-2020 the bootstrap authors (https://github.com/twbs/bootstrap/graphs/contributors)
+*    
+*    Licenses:
+*      MIT License
+*      SPDXId: MIT
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 package com.intuit.karate.gatling
 
 import io.gatling.core.session.Session
