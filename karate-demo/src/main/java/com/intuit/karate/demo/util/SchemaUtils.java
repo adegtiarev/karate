826b7f863383fe43052acf56dab626e37d8ef934
Andrei Degtiarev <adegtiarev@gmail.com>
2023-12-01 16:30:41 -0300

    update

--- karate-demo/src/main/java/com/intuit/karate/demo/util/SchemaUtils.java
+++ karate-demo/src/main/java/com/intuit/karate/demo/util/SchemaUtils.java
@@ -1,3 +1,24 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Project: https://karatelabs.github.io/karate
+*    Release: https://github.com/karatelabs/karate/releases/tag/v1.4.0
+*    Source File: SchemaUtils.java
+*    
+*    Copyrights:
+*      copyright (c) 2013 gopivotal, inc. all rights reserved
+*      copyright 2017 intuit inc
+*    
+*    Licenses:
+*      MIT License
+*      SPDXId: MIT
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 package com.intuit.karate.demo.util;
 
 import com.fasterxml.jackson.databind.JsonNode;
