d27d233a071e4ce4daaaca40241c8825e3886529
Andrei Degtiarev <adegtiarev@gmail.com>
2023-12-01 16:29:28 -0300

    update

--- karate-core/src/main/java/com/intuit/karate/report/karate-report.js
+++ karate-core/src/main/java/com/intuit/karate/report/karate-report.js
@@ -1,3 +1,23 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Project: https://karatelabs.github.io/karate
+*    Release: https://github.com/karatelabs/karate/releases/tag/v1.2.0.RC1
+*    Source File: karate-report.js
+*    
+*    Copyrights:
+*      copyright 2017 intuit inc
+*    
+*    Licenses:
+*      MIT License
+*      SPDXId: MIT
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 window.onload = function () {
   $('div.step-container').each(function (i) {
     var id = this.id;
